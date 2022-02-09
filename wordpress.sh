dedcfca7e9bf0aa5e3b6209b154cfba51a1deae2
Jeffrey <64604222+thewisejun@users.noreply.github.com>
2022-02-08 22:01:36 -0500

    Create wordpress.sh

---
+++ wordpress.sh
@@ -0,0 +1,53 @@
+sudo apt install software-properties-common
+
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt-get update
+
+
+sudo apt-get upgrade
+
+
+sudo apt-get install apache2
+
+
+sudo ufw app info "Apache Full"
+
+sudo apt-get install mysql-server
+
+
+mysql_secure_installation
+
+sudo apt install php7.4
+
+sudo apt install php7.4-mysql
+
+
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
+
+
+sudo systemctl restart apache2
+
+
+sudo apt-get install php-cli
+
+cd /var/www/html
+
+wget https://wordpress.org/latest.tar.gz
+
+tar -xzf latest.tar.gz
+
+rm latest.tar.gz
+
+cd wordpress
+
+cp -r * ../
+
+cd ..
+
+rm -r wordpress
+
+cd /var/www/html 
+
+chmod +x /var/www/html
+
